34091e6db0f8fde78fe8bbcafeae0265b5782327
PaHod <foxy.pahod@gmail.com>
2014-12-17 16:10:53 +0200

    dfgdf

---
+++ qaz/src/main/java/ghjkl/Qwerhjfhj.java
@@ -0,0 +1,10 @@
+package ghjkl;
+
+/**
+ * Created by PaHod on 17.12.2014.
+ */
+public class Qwerhjfhj {
+    public static void main(String[] args) {
+        System.out.println("lalalalalalal");
+    }
+}
